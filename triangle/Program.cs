233116a1d6daa14a949e92ce0c58323249dacfa4
Acer <Acer@DESKTOP-JS0O9TD>
2023-11-02 20:23:23 +0330

    Add project files.

---
+++ triangle/Program.cs
@@ -0,0 +1,12 @@
+﻿//نیما فدوی دنباله مثلثی
+
+Console.WriteLine("adad algo ra vared konid");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int x = 0;
+for (int i = 1; i <= number; i++)
+{
+    x = x + i;
+}
+Console.WriteLine(x);
+Console.ReadKey();
